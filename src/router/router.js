0252ccc8714a1441f0b7cd93825d7183e182cb8c
as <as@asdeiMac.local>
2020-08-27 18:49:47 +0800

    add router

---
+++ src/router/router.js
@@ -0,0 +1,45 @@
+import App from "../App.vue";
+
+const Home = () => import("../pages/home");
+const Login = () => import("../pages/login");
+const Order = () => import("../pages/order");
+const SettleAccounts = () => import("../pages/settleAccounts");
+const User = () => import("../pages/user");
+const NotFound = () => <tempalte>404</tempalte>;
+
+export default [
+  {
+    path: "/",
+    component: App,
+    children: [
+      {
+        path: "",
+        redirect: "/login",
+      },
+      {
+        path: "/home",
+        component: Home,
+      },
+      {
+        path: "/login",
+        component: Login,
+      },
+      {
+        path: "/order",
+        component: Order,
+      },
+      {
+        path: "/user",
+        component: User,
+      },
+      {
+        path: "/settleAccounts",
+        component: SettleAccounts,
+      },
+      {
+        path: "*",
+        component: NotFound,
+      },
+    ],
+  },
+];
