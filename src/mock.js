043365fd41a53e814c63e3301b348f5782b9dd8d
DouLu <luckyludou@gmail.com>
2020-12-01 22:57:17 +0800

    feat:createOrder completed

--- src/mock.js
+++ src/mock.js
@@ -1,20 +1,51 @@
 // 使用 Mock
 // https://github.com/nuysoft/Mock/wiki/Getting-Started
+// http://mockjs.com/examples.html
 const Mock = require('mockjs')
 const Random = Mock.Random
+Random.ctitle()
+Random.name(true)
+Random.cname(true)
+Random.guid()
 // 数据持久化   保存在global的全局变量中
-const data = {}
-const orderList = []
-for (let i = 0; i < 30; i++) {
-  const listObj = {
-    title: Random.csentence(5, 10),
-    orderId: Random.id(5, 10),
-    money: Random.integer(100, 9999),
-    photo: Random.image('114*83', '#00405d', '#fff', 'mock.js')
-  }
-  orderList.push(listObj)
+if (!global.data) {
+  const data = Mock.mock({
+    // 订单项信息
+    selectList: {
+      'brandList|1-10': [
+        {
+          // 属性 id 是一个自增数，起始值为 1，每次增 1
+          'bid|+1': 1,
+          name: '@name'
+        }
+      ],
+      'accessoryList|1-10': [
+        {
+          // 属性 id 是一个自增数，起始值为 1，每次增 1
+          'aid|+1': 1,
+          name: '@cname',
+          'model|1-10': [
+            {
+              'mid|+': 1,
+              name: '@guid'
+            }
+          ]
+        }
+      ]
+    },
+    // 订单列表
+    'orderList|1-10': [
+      {
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        'orderId|+1': 1,
+        title: '@ctitle',
+        photo: Random.image('200x100', '#00405d', '#FFF', 'Mock.js')
+      }
+    ]
+  })
+  global.data = data
 }
-data.orderList = orderList
+
 Mock.mock('/api/register', (req, res) => {
   console.log(req, res)
   return { data: true }
@@ -24,5 +55,11 @@ Mock.mock('/api/login', (req, res) => {
   return { data: data.user }
 })
 Mock.mock('/api/data', (req, res) => {
-  return { data: data.orderList }
+  return { data: global.data.orderList }
+})
+Mock.mock('/api/getSelInfo', () => {
+  return { data: global.data.selectList }
+})
+Mock.mock('/api/createOrder', () => {
+  return { data: true }
 })
--- src/view/createOrder.vue
+++ src/view/createOrder.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <p class="title">create order</p>
-    <el-form ref="orderForm" :model="orderForm" label-width="80px">
+    <el-form
+      ref="orderForm"
+      :model="orderForm"
+      label-width="80px"
+    >
       <el-form-item label="客户姓名">
         <el-input v-model="orderForm.name" />
       </el-form-item>
       <el-form-item label="品牌">
-        <el-select v-model="orderForm.brand" placeholder="请选择品牌">
-          <el-option label="区域一" value="shanghai" />
-          <el-option label="区域二" value="beijing" />
+        <el-select
+          v-model="orderForm.brand"
+          placeholder="请选择品牌"
+        >
+          <el-option
+            v-for="brand in brandList"
+            :key="brand.bid"
+            :label="brand.name"
+            :value="brand.bid"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="日期">
@@ -20,24 +31,50 @@
         />
       </el-form-item>
       <el-form-item label="配件名称">
-        <el-select v-model="orderForm.accessoryName" placeholder="请选择配件">
-          <el-option label="区域一" value="shanghai" />
-          <el-option label="区域二" value="beijing" />
+        <el-select
+          v-model="orderForm.accessoryName"
+          placeholder="请选择配件"
+          @change="selAccessory"
+        >
+          <el-option
+            v-for="a in accessoryList"
+            :key="a.aid"
+            :label="a.name"
+            :value="a.aid"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="配件型号">
+        <!-- TODO: bug 切换配件名称，配件型号列表里只有一个时 型号下拉选项的值是上一个的值 -->
         <el-select
           v-model="orderForm.accessoryModel"
           placeholder="请选择配件型号"
         >
-          <el-option label="区域一" value="shanghai" />
-          <el-option label="区域二" value="beijing" />
+          <el-option
+            v-for="m in modelList"
+            :key="m.mid"
+            :label="m.name"
+            :value="m.mid"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="单位">
-        <el-select v-model="orderForm.unit" placeholder="请选择单位">
-          <el-option label="区域一" value="shanghai" />
-          <el-option label="区域二" value="beijing" />
+        <el-select
+          v-model="orderForm.unit"
+          placeholder="请选择单位"
+        >
+          <el-option
+            label="个"
+            value="个"
+          />
+          <el-option
+            label="台"
+            value="台"
+          />
+          <el-option
+            label="件"
+            value="件"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="数量">
@@ -57,31 +94,56 @@
         />
       </el-form-item>
       <el-form-item label="客户要求">
-        <el-input v-model="orderForm.desc" type="textarea" />
+        <el-input
+          v-model="orderForm.desc"
+          type="textarea"
+        />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button
+          type="primary"
+          @click="onSubmit"
+        >立即创建</el-button>
         <el-button>保存</el-button>
-        <el-button>重置数据</el-button>
+        <el-button @click="reset">重置数据</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data: function() {
     return {
-      orderForm: {}
+      orderForm: {},
+      brandList: [],
+      accessoryList: [],
+      modelList: []
     }
   },
   beforeMount: function() {
     // fetch select data
-
+    axios.get('/api/getSelInfo').then(res => {
+      const { brandList, accessoryList } = res.data.data
+      this.brandList = brandList
+      this.accessoryList = accessoryList
+    })
   },
   methods: {
+    selAccessory: function(value) {
+      const accessory = this.accessoryList.filter(i => i.aid === value)[0]
+      this.modelList = accessory.model
+    },
     onSubmit: function() {
-      // TODO: validate
+      // TODO: form rules validate
+      console.log('orderForm data-------', this.orderForm)
+      axios.post('/api/createOrder').then(res => {
+        if (res.data.data) this.$router.push('/orderList')
+      })
+    },
+    reset: function() {
+      this.orderForm = {}
     }
   }
 }
