d69742a583a5be25c8a24fe5b4a4ccdab96d2244
DL <2735413301@qq.com>
2021-10-20 15:47:19 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,29 @@ import VueRouter from 'vue-router'
 import router from './router/router'
 import store from './store/index'
 require('./mock')
+import * as Sentry from '@sentry/vue'
+import { Integrations } from '@sentry/tracing'
 
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+
+// if (process.env.NODE_ENV !== 'development') {
+// }
+Sentry.init({
+  Vue,
+  dsn: 'https://db45f1b887f94f4884cb3cb48dc2f715@o1015021.ingest.sentry.io/5980315',
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ['localhost', 'my-site-url.com', /^\//]
+    })
+  ],
+  tracesSampleRate: 1.0,
+  release: 'dl-test5.0'
+})
 
 new Vue({
   router,
